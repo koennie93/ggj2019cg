317b528d04c39d9ced2399f5a98c79528ef2881f
Robin Maree <robin@robinmaree.com>
2019-01-25 21:26:53 +0100

    UI test.

--- ggj-winning/Assets/Scripts/UIManager.cs
+++ ggj-winning/Assets/Scripts/UIManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class placeholder4 : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
