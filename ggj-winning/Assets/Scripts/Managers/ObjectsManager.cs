54fd04fb910b54a0f3ee3c67c02ac080d53b4731
Koen van Rijn <koenvanrijn21@gmail.com>
2019-01-25 21:58:37 +0100

    Object Controller start

---
+++ ggj-winning/Assets/Scripts/Managers/ObjectsManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsManager : MonoBehaviour
+{
+    [HideInInspector] public static ObjectsManager Instance = null;
+
+    [Serializable]
+    public struct Object
+    {
+        public string name;
+        public ObjectController objectItem;
+    }
+
+    public Object[] tutorials;
+    public Dictionary<string, ObjectController> tutorialDictionary = new Dictionary<string, ObjectController>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ggj-winning/Assets/Scripts/ObjectController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ObjectController : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer sprite;
+
+    private float currentHP;
+    private float dmgPerHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            BeginShake();
+        }
+    }
+
+    void BeginShake()
+    {
+        //sprite.transform.DOMoveX(5, 2);
+        //sprite.transform.DOMoveX(0, 1).From();
+        Sequence s = DOTween.Sequence();
+
+        s.Append(transform.DORotate(new Vector3(0, 50, 0), 2));
+    }
+}
