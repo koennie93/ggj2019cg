77cb80ec96964a85fe295425f84735e6caa52dec
Robin Maree <robin@robinmaree.com>
2019-01-25 21:56:44 +0100

    Script for UI.

--- ggj-winning/Assets/Scripts/UIManager.cs
+++ ggj-winning/Assets/Scripts/UIManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public Text playerOneText;
+    public Text playerTwoText;
+
+    public Text playerOneScore;
+    public Text playerTwoScore;
+
+    public string message;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerOneScore.text = "0";
+        playerTwoScore.text = "0";
+
+        SetPlayerOneScore("1000");
+    }
+
+    void SetPlayerOneScore(string score)
+    {
+        playerOneScore.text = "";
+        StartCoroutine(TypeText(score, playerOneScore));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    IEnumerator TypeText(string text, Text textElement)
+    {
+        foreach (char letter in text.ToCharArray())
+        {
+            textElement.text += letter;
+
+            yield return new WaitForSeconds(1);
+        }
     }
 }
