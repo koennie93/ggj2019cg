270acbfc6cfdf790e3d4c323b487969e7f48e601
Robin Maree <robin@robinmaree.com>
2019-01-26 13:03:07 +0100

    Added DOTween library.

--- ggj-winning/Assets/Scripts/UIManager.cs
+++ ggj-winning/Assets/Scripts/UIManager.cs
@@ -5,14 +5,9 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public Text playerOneText;
-    public Text playerTwoText;
-
     public Text playerOneScore;
     public Text playerTwoScore;
-
-    public Vector3 newPosition;
-
+    
     public string message;
 
     // Start is called before the first frame update
@@ -20,32 +15,21 @@ public class UIManager : MonoBehaviour
     {
         playerOneScore.text = "0";
         playerTwoScore.text = "0";
-
-        SetPlayerOneScore("1000");
     }
 
-    void SetPlayerOneScore(string score)
+    public void SetPlayerOneScore(string score)
     {
-        playerOneScore.text = "";
-        StartCoroutine(TypeText(score, playerOneScore));
+        playerOneScore.text = score;
+    }
+
+    public void SetPlayerTwoScore(string score)
+    {
+        playerTwoScore.text = score;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            playerOneText.transform.position += newPosition;
-        }
     }
 
-    IEnumerator TypeText(string text, Text textElement)
-    {
-        foreach (char letter in text.ToCharArray())
-        {
-            textElement.text += letter;
-
-            yield return new WaitForSeconds(1);
-        }
-    }
 }
