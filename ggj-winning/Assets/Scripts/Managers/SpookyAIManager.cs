d065309908774ce5b1593706b25decf1298535ee
DESKTOP-BVUCGQO\Lourens <l3.kauf@gmail.com>
2019-01-26 05:24:21 +0100

    Spooky AI added

---
+++ ggj-winning/Assets/Scripts/Managers/SpookyAIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpookyAIManager : MonoBehaviour
+{
+    [Range(1, 10)] public int maxSimultaneousAttacks = 3;
+    [Range(1.0f, 12.0f)] public float minimumAttackDelay = 4.0f, maximumAttackDelay = 8.0f;
+
+    private ObjectController[] allObjects;
+    private Coroutine ai;
+
+    private void Awake()
+    {
+        allObjects = FindObjectsOfType<ObjectController>();
+        StartAI();
+        StopAI();
+    }
+
+    public void StartAI()
+    {
+        StopAI();
+        ai = StartCoroutine(SelectObjects(minimumAttackDelay, maximumAttackDelay, 5));
+    }
+
+    public void StopAI()
+    {
+        if (ai == null)
+            return;
+
+        StopCoroutine(ai);
+        ai = null;
+    }
+
+    private IEnumerator SelectObjects(float minDelay, float maxDelay, int attackAmount)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+            ObjectController[] idleObjects = allObjects.Where(obj => obj.state == ObjectState.IDLE).ToArray();
+            int shookObjectsAmount = allObjects.Where(obj => obj.state == ObjectState.SHOOK).Count();
+            if (idleObjects.Length > 0 && shookObjectsAmount < maxSimultaneousAttacks)
+                idleObjects[Random.Range(0, idleObjects.Length)].BeginShake(attackAmount);
+        }
+    }
+}
--- ggj-winning/Assets/Scripts/ObjectController.cs
+++ ggj-winning/Assets/Scripts/ObjectController.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+public enum ObjectState
+{
+    IDLE,
+    SHOOK,
+    DESTROYED
+}
+
 public class ObjectController : MonoBehaviour
 {
     [SerializeField]
@@ -21,9 +28,13 @@ public class ObjectController : MonoBehaviour
     [SerializeField]
     private float dmgPerHit;
 
+    [HideInInspector]
+    public ObjectState state;
+
     // Start is called before the first frame update
     void Start()
     {
+        state = ObjectState.IDLE;
         currentHP = MaxHP;
         HPText.text = currentHP + "/" + MaxHP;
     }
@@ -33,7 +44,7 @@ public class ObjectController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            BeginShake();
+            BeginShake(0);
         }
         if (Input.GetKeyDown(KeyCode.I))
         {
@@ -45,26 +56,53 @@ public class ObjectController : MonoBehaviour
         }
     }
 
-    void BeginShake()
+    public void BeginShake(int attackAmount)
     {
+        state = ObjectState.SHOOK;
         originalRotation = sprite.transform.localRotation.eulerAngles;
         Sequence s = DOTween.Sequence();
 
         s.Append(sprite.transform.DORotate(originalRotation + endRotation, shakeDuration).SetEase(Ease.InQuad).SetLoops(5, LoopType.Yoyo));
         s.Append(sprite.transform.DORotate(originalRotation, shakeDuration));
-       
+
+        StartCoroutine(Shake(attackAmount));
     }
 
-    void ChangeHP(float addedValue)
+    public void ChangeHP(float addedValue)
     {
         currentHP += addedValue;
         if (currentHP > MaxHP) currentHP = MaxHP;
-        if (currentHP < 0)
+        if (currentHP <= 0)
         {
+            state = ObjectState.DESTROYED;
             currentHP = 0;
             //TO-DO Call Destroy? method.
         }
 
-        HPText.text = currentHP + "/" + MaxHP;
+        HPText.text = Mathf.Round(currentHP) + "/" + MaxHP;
     }
+
+    private IEnumerator Shake(int attackAmount)
+    {
+        while(attackAmount > 0)
+        {
+            yield return new WaitForSeconds(3);
+            attackAmount--;
+            ChangeHP(-dmgPerHit);
+
+            originalRotation = sprite.transform.localRotation.eulerAngles;
+            Sequence s = DOTween.Sequence();
+            s.Append(sprite.transform.DORotate(originalRotation + endRotation / 3, shakeDuration).SetEase(Ease.InQuad).SetLoops(5, LoopType.Yoyo));
+            s.Append(sprite.transform.DORotate(originalRotation, shakeDuration));
+
+            if (state == ObjectState.DESTROYED)
+                break;
+        }
+
+        yield return new WaitForSeconds(3);
+
+        if (state != ObjectState.DESTROYED)
+            state = ObjectState.IDLE;
+    }
+    
 }
